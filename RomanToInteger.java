53d4104ad7b0fcbaa0dca527f58af0fb28d336eb
Vighnesh280 <109091174+Vighnesh280@users.noreply.github.com>
2024-11-22 14:53:32 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String roman = "IX"; // Example input
+        int result = romanToInt(roman);
+        System.out.println("Integer value of Roman numeral " + roman + ": " + result);
+    }
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+}
